0f90f89976969e9a5f372f10dc806b891f789f35
Jeff Davies <hipparchus2000@gmail.com>
2018-01-23 22:59:47 +0000

    initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,197 @@
+﻿using System.Windows.Forms;
+
+namespace dupes
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.TreeNode treeNode2 = new System.Windows.Forms.TreeNode("Root");
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.baseDirectory = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label3 = new System.Windows.Forms.Label();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.deleteToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.button3 = new System.Windows.Forms.Button();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // baseDirectory
+            // 
+            this.baseDirectory.Location = new System.Drawing.Point(25, 39);
+            this.baseDirectory.Name = "baseDirectory";
+            this.baseDirectory.Size = new System.Drawing.Size(672, 20);
+            this.baseDirectory.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(22, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(76, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Base Directory";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label2.Location = new System.Drawing.Point(50, 50);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(76, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "files";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(738, 36);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Change";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(25, 76);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "find dupes";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // treeView1
+            // 
+            this.treeView1.Location = new System.Drawing.Point(25, 105);
+            this.treeView1.Name = "treeView1";
+            treeNode2.ContextMenuStrip = this.contextMenuStrip1;
+            treeNode2.Name = "Root";
+            treeNode2.Tag = "root";
+            treeNode2.Text = "Root";
+            this.treeView1.Nodes.AddRange(new System.Windows.Forms.TreeNode[] {
+            treeNode2});
+            this.treeView1.Size = new System.Drawing.Size(897, 262);
+            this.treeView1.TabIndex = 4;
+            this.treeView1.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.treeView1_AfterSelect);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(548, 76);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "label3";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItem1,
+            this.deleteToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(108, 48);
+            this.contextMenuStrip1.Text = "File";
+            this.contextMenuStrip1.Opening += new System.ComponentModel.CancelEventHandler(this.contextMenuStrip1_Opening);
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(152, 22);
+            this.toolStripMenuItem1.Text = "File";
+            // 
+            // deleteToolStripMenuItem
+            // 
+            this.deleteToolStripMenuItem.Name = "deleteToolStripMenuItem";
+            this.deleteToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.deleteToolStripMenuItem.Text = "Delete";
+            this.deleteToolStripMenuItem.Click += new System.EventHandler(this.deleteToolStripMenuItem_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(786, 78);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(73, 20);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "delete";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(934, 393);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.treeView1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.baseDirectory);
+            this.Name = "Form1";
+            this.Text = "Dupes";
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.TextBox baseDirectory;
+        private System.Windows.Forms.Label label1;
+        private Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.Timer timer1;
+        private Label label3;
+        private ContextMenuStrip contextMenuStrip1;
+        private ToolStripMenuItem toolStripMenuItem1;
+        private ToolStripMenuItem deleteToolStripMenuItem;
+        private Button button3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,235 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace dupes
+{
+    
+
+    public partial class Form1 : Form
+    {
+        public class myEntry
+        {
+            public myEntry(string t, string Sha, long size)
+            {
+                text = t;
+                visible = false;
+                Size = size;
+                sha = Sha;
+            }
+            public string text { get; set; }
+            public bool visible { get; set; }
+            public long Size { get; set; }
+            public string sha { get; set; }
+        }
+
+        public class dupe
+        {
+            public string sha { get; set; }
+            public List<string> locations { get; set; }
+            public long Size { get; internal set; }
+        }
+
+        List<myEntry> list;
+        
+        long myCount = 0;
+        private bool enableDisplay;
+        private List<dupe> dupeList;
+
+        public Form1()
+        {
+            InitializeComponent();
+            enableDisplay = false;
+            myCount = 0;
+            list = new List<myEntry>();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            folderBrowserDialog1.RootFolder = new Environment.SpecialFolder();
+            folderBrowserDialog1.ShowDialog();
+            var path = folderBrowserDialog1.SelectedPath;
+            baseDirectory.Text = path;
+
+        }
+
+        private static string GetChecksum(string file)
+        {
+            using (FileStream stream = File.OpenRead(file))
+            {
+                var sha = new SHA256Managed();
+                byte[] checksum = sha.ComputeHash(stream);
+                return BitConverter.ToString(checksum).Replace("-", String.Empty);
+            }
+        }
+
+        private static string GetChecksumBuffered(Stream stream)
+        {
+            using (var bufferedStream = new BufferedStream(stream, 1024 * 32))
+            {
+                var sha = new SHA256Managed();
+                byte[] checksum = sha.ComputeHash(bufferedStream);
+                return BitConverter.ToString(checksum).Replace("-", String.Empty);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            treeView1.Nodes.Clear();
+
+            var di = new DirectoryInfo(baseDirectory.Text);
+            new Thread(() =>
+            {
+                Thread.CurrentThread.IsBackground = true;
+                myCount = 0;
+                list = new List<myEntry>();
+                WalkDirectoryTree(di, countFile);
+                WalkDirectoryTree(di, shaFile);
+                //group the files by sha into dupeList
+                dupeList = new List<dupe>();
+                foreach (var fileEntry in list.OrderBy(x => x.sha))
+                {
+                    var existingDupe = dupeList.SingleOrDefault(x => x.sha == fileEntry.sha);
+                    if (existingDupe == null)
+                    {
+                        dupeList.Add(new dupe
+                        {
+                            locations = new List<string> { fileEntry.text },
+                            sha = fileEntry.sha,
+                            Size = fileEntry.Size
+                        });
+
+                    } else
+                    {
+                        existingDupe.locations.Add(fileEntry.text);
+                    }
+                }
+                //prune entries with 1 file
+                foreach (var dupeEntry in dupeList.ToArray())
+                {
+                    if (dupeEntry.locations.Count == 1)
+                    {
+                        dupeList.Remove(dupeEntry);
+                    }
+                }
+
+                //display them
+                enableDisplay = true;
+                
+
+            }).Start();
+
+        }
+
+        public delegate void MyFunction(FileInfo fi);
+
+        public void WalkDirectoryTree(System.IO.DirectoryInfo root, MyFunction func)
+        {
+            System.IO.FileInfo[] files = null;
+            System.IO.DirectoryInfo[] subDirs = null;
+
+            files = root.GetFiles("*.*");
+
+            if (files != null)
+            {
+                foreach (System.IO.FileInfo fi in files)
+                {
+                    func(fi);
+                }
+
+                subDirs = root.GetDirectories();
+
+                foreach (System.IO.DirectoryInfo dirInfo in subDirs)
+                {
+                    WalkDirectoryTree(dirInfo, func);
+                }
+            }
+        }
+
+        private void countFile(FileInfo fi)
+        {
+            myCount++;
+        }
+
+        private void shaFile(FileInfo fi)
+        {
+            var filePath = fi.FullName;
+            var sha = GetChecksum(filePath);
+            var size = fi.Length;
+            list.Add(new myEntry( filePath , sha, size ));
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (enableDisplay == true)
+            {
+                foreach (var dupeEntry in dupeList.OrderByDescending(x=>x.Size).ToArray())
+                {
+                    TreeNode node = treeView1.Nodes.Add(dupeEntry.Size+"  "+dupeEntry.sha);
+                    foreach (var location in dupeEntry.locations)
+                    {
+                        node.Nodes.Add(location);
+                    }
+                    dupeList.Remove(dupeEntry);
+                }
+                enableDisplay = false;
+            }
+            else
+            {
+                label3.Text = myCount.ToString() + " files found  " + list.Count().ToString() + " files hashed";
+            }
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void deleteToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            var hitTest = treeView1.HitTest(treeView1.PointToClient(new Point(contextMenuStrip1.Left, contextMenuStrip1.Top)));
+            if (hitTest.Node != null)
+            {
+                DialogResult dialogResult = MessageBox.Show("Delete", hitTest.Node.Text, MessageBoxButtons.YesNo);
+                if (dialogResult == DialogResult.Yes)
+                {
+                    File.Delete(hitTest.Node.Text);
+                }
+            }
+
+        }
+
+        private void treeView1_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+
+        }
+
+        private void contextMenuStrip1_Opening(object sender, CancelEventArgs e)
+        {
+
+        }
+        
+        private void button3_Click(object sender, EventArgs e)
+        {
+            DialogResult dialogResult = MessageBox.Show("Delete", treeView1.SelectedNode.Text, MessageBoxButtons.YesNo);
+            if (dialogResult == DialogResult.Yes)
+            {
+                File.Delete(treeView1.SelectedNode.Text);
+            }
+
+            
+        }
+    }
+
+
+
+}
